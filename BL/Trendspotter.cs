eda50b168a47fa14e5f9a56383436b3e8b132c84
ThomasVerhoeven1998 <35959140+ThomasVerhoeven1998@users.noreply.github.com>
2018-03-11 17:07:37 +0100

    Trendspotter
    Nieuwe klasse Thomas
    +
    newest JSON package

---
+++ BL/Trendspotter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB.BL
+{
+  public class Trendspotter
+  {
+
+    //generateAlerts moeten net andersom, De user moet een methode kunnen oproepen die alerts genereert (want is customizable per user) 
+    //bv. user1.generateAlerts()
+    //dus frequentie bij houden en for each op users en daarop oproepen
+    //wat is dictionary (soort van collection)
+  }
+}
