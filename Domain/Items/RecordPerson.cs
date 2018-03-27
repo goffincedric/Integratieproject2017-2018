746c179a2aa450776036763154c7a537a092cd23
goffincedric <goffincedric@users.noreply.github.com>
2018-03-27 21:13:23 +0200

    Signup fix + Recordperson fix

--- Domain/Items/RecordPerson.cs
+++ Domain/Items/RecordPerson.cs
@@ -9,35 +9,37 @@ using System.Threading.Tasks;
 namespace Domain.Items
 {
 
-  [Table("tblPersonRecord")]
-  public class RecordPerson
+    [Table("tblRecordPerson")]
+    public class RecordPerson
 
-  {
-    [Key]
-    public int Number { get; set; }
-
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-
-    public override string ToString()
     {
-      return FirstName + " " + LastName;
-    }
+        [Key]
+        public int Id { get; set; }
 
-    public override bool Equals(object obj)
-    {
-      if (!(obj is RecordPerson)) return false;
-      RecordPerson rp = (RecordPerson)obj;
-      if (rp.FirstName.ToLower().Equals(this.FirstName.ToLower()) && rp.LastName.ToLower().Equals(this.LastName.ToLower())) return true;
-      return false;
-    }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
-    public override int GetHashCode()
-    {
-      var hashCode = 1938039292;
-      hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(FirstName);
-      hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(LastName);
-      return hashCode;
+        public List<Record> Records { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is RecordPerson)) return false;
+            RecordPerson rp = (RecordPerson)obj;
+            if (rp.FirstName.ToLower().Equals(this.FirstName.ToLower()) && rp.LastName.ToLower().Equals(this.LastName.ToLower())) return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1938039292;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(FirstName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(LastName);
+            return hashCode;
+        }
     }
-  }
 }
