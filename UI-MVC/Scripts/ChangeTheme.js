46de7f268acf057a4949d84623ea6e179abde941
LinsVN <36882455+LinsVN@users.noreply.github.com>
2018-04-05 23:54:05 +0200

    Light-Dark-Switch

--- UI-MVC/Scripts/ChangeTheme.js
+++ UI-MVC/Scripts/ChangeTheme.js
@@ -1,25 +1,14 @@
 ﻿﻿$(document).ready(function () {
-  $("#dark-mode").hover(function () {
-    $(".dropdown-menu").css("background-color", "#333333");
-    $(".dropdown-menu").css("color", "white");
-    $(".c-grey-900").css("color", "white !important");
-  }, function () {
-    $(".dropdown-menu").css("background-color", "");
-    $(".dropdown-menu").css("color", "");
-    $(".c-grey-900").css("color", "");
-  });
+    $("#dark-mode").click(function () {
+        $("#ThemeLink").attr("href", "/Content/Theme/DarkMode.css")
+    })
 }),
-  $(document).ready(function () {
-    $("#light-mode").hover(function () {
-      $(".dropdown-menu").css("background-color", "white");
-    }, function () {
-      $(".dropdown-menu").css("background-color", "");
+    $(document).ready(function () {
+        $("#light-mode").click(function () {
+            $("#ThemeLink").attr("href", "/Content/Theme/LightMode.css")
+        })
+    }), $(document).ready(function () {
+        $("#future-mode").click(function () {
+            $("#ThemeLink").attr("href", "/Content/Theme/FutureMode.css")
+        })
     });
-  }),
-  $(document).ready(function () {
-    $("#future-mode").hover(function () {
-      $(".dropdown-menu").css("background", "linear-gradient(to top left, rgba(0,0,0,0.125), rgba(0,0,0,0.25))");
-    }, function () {
-      $(".dropdown-menu").css("background", "");
-    });
-  });
