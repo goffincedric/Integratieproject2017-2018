6a8f10cdc27c39067e2bdaa007844a5c1a0193fe
goffincedric <goffincedric@users.noreply.github.com>
2018-04-12 15:22:44 +0200

    Basic structrure backend Dashboard

---
+++ DAL/DashboardRepo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PB.BL.Domain.Dashboards;
+using PB.DAL.EF;
+
+namespace PB.DAL
+{
+    public class DashboardRepo : IDashboardRepo
+    {
+        private IntegratieDbContext ctx;
+
+        public DashboardRepo()
+        {
+            ctx = new IntegratieDbContext();
+        }
+
+        public DashboardRepo(UnitOfWork uow)
+        {
+            ctx = uow.Context;
+        }
+
+        public IEnumerable<Dashboard> CreateDashboards(List<Dashboard> dashboards)
+        {
+            ctx.Dashboards.AddRange(dashboards);
+            ctx.SaveChanges();
+            return dashboards;
+        }
+
+        public Dashboard CreateDashboard(Dashboard dashboard)
+        {
+            ctx.Dashboards.Add(dashboard);
+            ctx.SaveChanges();
+            return dashboard;
+        }
+
+        public void DeleteDashboard(int dashboardId)
+        {
+            Dashboard dashboard = ReadDashboard(dashboardId);
+            ctx.Dashboards.Remove(dashboard);
+            ctx.SaveChanges();
+        }
+
+        public Dashboard ReadDashboard(int dashboardId)
+        {
+            return ctx.Dashboards
+                .Include("Zones")
+                .FirstOrDefault(d => d.DashboardId == dashboardId);
+        }
+
+        public IEnumerable<Dashboard> ReadDashboards()
+        {
+            return ctx.Dashboards
+                .Include("Zones")
+                .AsEnumerable();
+        }
+
+        public void UpdateDashboard(Dashboard dashboard)
+        {
+            ctx.Dashboards.Attach(dashboard);
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ DAL/IDashboardRepo.cs
@@ -0,0 +1,19 @@
+﻿using PB.BL.Domain.Dashboards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB.DAL
+{
+    public interface IDashboardRepo
+    {
+        IEnumerable<Dashboard> ReadDashboards();
+        Dashboard CreateDashboard(Dashboard dashboard);
+        IEnumerable<Dashboard> CreateDashboards(List<Dashboard> dashboards);
+        Dashboard ReadDashboard(int dashboardId);
+        void UpdateDashboard(Dashboard dashboard);
+        void DeleteDashboard(int dashboardId);
+    }
+}
