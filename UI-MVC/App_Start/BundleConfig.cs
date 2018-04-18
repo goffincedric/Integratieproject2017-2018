f4f72e5424df9d02b7bf1a4c4164b828f51d5cc1
LinsVN <36882455+LinsVN@users.noreply.github.com>
2018-04-18 16:44:22 +0200

    Everything except change password

--- UI-MVC/App_Start/BundleConfig.cs
+++ UI-MVC/App_Start/BundleConfig.cs
@@ -3,28 +3,29 @@ using System.Web.Optimization;
 
 namespace UI_MVC
 {
-  public class BundleConfig
-  {
-    // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
-    public static void RegisterBundles(BundleCollection bundles)
+    public class BundleConfig
     {
-      bundles.IgnoreList.Clear();
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
 
-      bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                  "~/Scripts/jquery-3.3.1.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.3.1.js"));
 
-      bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
-                  "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
+                        "~/Scripts/bootstrap.js"));
 
-      bundles.Add(new ScriptBundle("~/bundles/adminator").Include("~/Scripts/load.js","~/Scripts/vendor.js", "~/Scripts/bundle.js"));
+            bundles.Add(new ScriptBundle("~/bundles/adminator").Include("~/Scripts/load.js", "~/Scripts/vendor.js", "~/Scripts/bundle.js"));
 
-      bundles.Add(new ScriptBundle("~/bundles/changetheme").Include("~/Scripts/Changetheme.js"));
+            bundles.Add(new ScriptBundle("~/bundles/changetheme").Include("~/Scripts/Changetheme.js"));
 
-      bundles.Add(new ScriptBundle("~/bundles/contact").Include("~/Scripts/contact.js"));
 
-      bundles.Add(new ScriptBundle("~/bundles/hidecontent").Include("~/Scripts/hidecontent.js"));
+            bundles.Add(new ScriptBundle("~/bundles/contact").Include("~/Scripts/contact.js"));
 
-      bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include("~/Content/bootstrap.css"));
+            bundles.Add(new ScriptBundle("~/bundles/HideContentAccount").Include("~/Scripts/HideContentAccount.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include("~/Content/bootstrap.css"));
+        }
     }
-  }
 }
