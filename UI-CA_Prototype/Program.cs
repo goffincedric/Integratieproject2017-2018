1bf2fe8b5064851d34789e1f8e17ef7d5c9bbbd9
ThomasVerhoeven1998 <35959140+ThomasVerhoeven1998@users.noreply.github.com>
2018-04-25 08:38:54 +0200

    EERSTE GRAPH

--- UI-CA_Prototype/Program.cs
+++ UI-CA_Prototype/Program.cs
@@ -79,7 +79,7 @@ namespace UI_CA_Prototype
             do
             {
                 Console.Write("Keuze: ");
-                int.TryParse(Console.ReadLine(), out int keuze);
+               int.TryParse(Console.ReadLine(), out int keuze);
                 Console.WriteLine("\n");
 
                 switch (keuze)
