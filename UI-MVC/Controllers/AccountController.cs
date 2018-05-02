3fefed0cbbf09281a5eba95e5100fa0bf2187b30
ThomasVerhoeven1998 <35959140+ThomasVerhoeven1998@users.noreply.github.com>
2018-05-02 20:42:28 +0200

    added account settings page

--- UI-MVC/Controllers/AccountController.cs
+++ UI-MVC/Controllers/AccountController.cs
@@ -386,6 +386,11 @@ namespace UI_MVC.Controllers
             return View(profiles);
         }
 
+        public ViewResult UserSettings()
+        {
+            IEnumerable<Item> Subscriptions = UserManager.GetProfile(User.Identity.GetUserId()).Subscriptions;
+            return View(Subscriptions);
+        }
         #region ExternalLogin
         [HttpPost]
         [AllowAnonymous]
