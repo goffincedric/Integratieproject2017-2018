c5da6a53d670fba300a457895dc1a442d8d0d11f
goffincedric <goffincedric@users.noreply.github.com>
2018-05-09 14:43:55 +0200

    Temp (outdated model)

--- DAL/Interfaces/ISubPlatformRepo.cs
+++ DAL/Interfaces/ISubPlatformRepo.cs
@@ -11,5 +11,21 @@ namespace PB.DAL
         Subplatform ReadSubplatform(string subplatformURL);
         void UpdateSubplatform(Subplatform subplatform);
         void DeleteSubplatform(int subplatformId);
+
+        SubplatformSetting CreateSubplatformSetting();
+        void UpdateSubplatformSetting(SubplatformSetting subplatformSetting);
+
+        IEnumerable<Tag> ReadTags();
+        Subplatform CreateTag(Tag tagId);
+        Subplatform ReadTag(int tagId);
+        void UpdateTag(Tag subplatform);
+        void DeleteTag(int subplatformId);
+
+        IEnumerable<Subplatform> ReadPages();
+        Subplatform CreatePage(Subplatform subplatform);
+        Subplatform ReadPage(int subplatformId);
+        Subplatform ReadPage(string subplatformURL);
+        void UpdatePage(Subplatform subplatform);
+        void DeletePage(int subplatformId);
     }
 }
