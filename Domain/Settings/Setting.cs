8c71adbdf5bebc88fed417bcb1943bd34873d150
ThomasVerhoeven1998 <35959140+ThomasVerhoeven1998@users.noreply.github.com>
2018-06-11 15:35:14 +0200

    f

--- Domain/Settings/Setting.cs
+++ Domain/Settings/Setting.cs
@@ -24,7 +24,9 @@
             SITE_NAME,
             SEED_INTERVAL_HOURS,
             ALERT_GENERATION_INTERVAL_HOURS,
-            SEND_WEEKLY_REVIEWS_INTERVAL_DAYS
+            SEND_WEEKLY_REVIEWS_INTERVAL_DAYS,
+            PRIMARY_COLOR,
+            SECONDARY_COLOR
         }
     }
 }
